c7a4c892fc537292141f37a2475d9c567fbdf12f
cheongalc <22002891+cheongalc@users.noreply.github.com>
2023-08-24 16:52:42 +0800

    added autoregressive generation

--- models/transformer.py
+++ models/transformer.py
@@ -152,28 +152,75 @@ class Transformer(nn.Module):
 		if config.weight_tying:
 			self.lm_head.weight = self.decoder_token_embedding.weight
 
-	def forward(self, encoder_x, decoder_x, encoder_token_attn_mask, decoder_token_attn_mask):
+	def forward_encoder(self, encoder_x, encoder_token_attn_mask=None):
 		B, encoder_T = encoder_x.size()
-		_, decoder_T = decoder_x.size()
-		assert (encoder_T <= self.config.context_length) and (decoder_T <= self.config.context_length)
-
-		# forward pass through encoder
+		assert encoder_T <= self.config.context_length
+		
 		encoder_x_tok = self.encoder_token_embedding(encoder_x) # (B,encoder_T) -> (B,encoder_T,embed_dim)
 		encoder_x_pos = self.sinusoidal_positional_encoding(torch.arange(encoder_T, device=encoder_x.device)) # (encoder_T,embed_dim)
 		encoder_x_pos = torch.repeat_interleave(encoder_x_pos.unsqueeze(0), B, dim=0) # (encoder_T,embed_dim) -> (B,encoder_T,embed_dim)
 		encoder_x = self.dropout(encoder_x_tok + encoder_x_pos) # (B,encoder_T,embed_dim)
 		for encoder_block in self.encoder_blocks:
-			encoder_x = encoder_block(encoder_x, encoder_token_attn_mask) # (B,encoder_T,embed_dim)
+			encoder_x = encoder_block(encoder_x, 
+			     					  encoder_token_attn_mask=encoder_token_attn_mask) # (B,encoder_T,embed_dim)
 		
-		# forward pass through decoder
+		return encoder_x # (B,encoder_T,embed_dim)
+
+	def forward_decoder(self, encoder_outputs, decoder_x, encoder_token_attn_mask=None, decoder_token_attn_mask=None):
+		B, decoder_T = decoder_x.size()
+		assert decoder_T <= self.config.context_length
+
 		decoder_x_tok = self.decoder_token_embedding(decoder_x) # (B,decoder_T) -> (B,decoder_T,embed_dim)
 		decoder_x_pos = self.sinusoidal_positional_encoding(torch.arange(decoder_T, device=decoder_x.device)) # (decoder_T,embed_dim)
 		decoder_x_pos = torch.repeat_interleave(decoder_x_pos.unsqueeze(0), B, dim=0) # (decoder_T,embed_dim) -> (B,decoder_T,embed_dim)
 		decoder_x = self.dropout(decoder_x_tok + decoder_x_pos) # (B,decoder_T,embed_dim)
 		for decoder_block in self.decoder_blocks:
-			decoder_x = decoder_block(decoder_x, encoder_x, decoder_token_attn_mask, encoder_token_attn_mask) # (B,decoder_T,embed_dim)
-
-		# final language model head
+			decoder_x = decoder_block(decoder_x, 
+			     					  encoder_outputs, 
+									  decoder_token_attn_mask=decoder_token_attn_mask, 
+									  encoder_token_attn_mask=encoder_token_attn_mask) # (B,decoder_T,embed_dim)
 		logits = self.lm_head(decoder_x) # (B,decoder_T,embed_dim) -> (B,decoder_T,decoder_vocab_size)
-		
 		return logits
+
+	def forward(self, encoder_x, decoder_x, encoder_token_attn_mask=None, decoder_token_attn_mask=None):
+		encoder_outputs = self.forward_encoder(encoder_x, encoder_token_attn_mask=encoder_token_attn_mask) # (B,encoder_T,embed_dim)
+		logits = self.forward_decoder(encoder_outputs, 
+									  decoder_x, 
+									  encoder_token_attn_mask=encoder_token_attn_mask,
+									  decoder_token_attn_mask=decoder_token_attn_mask) # (B,decoder_T,decoder_vocab_size)
+		return logits
+	
+	@torch.inference_mode()
+	def generate_with_sampling(self, idxs, bos_idx:int, eos_idx:int, temperature:float=1.0, max_new_tokens:int=1000, sampling_strategy:str="multinomial"):
+		assert sampling_strategy in ["multinomial", "greedy"], f"Invalid sampling strategy: {sampling_strategy}"
+		
+		# put model into eval mode
+		self.eval()
+
+		# send the input into the model
+		encoder_x = idxs.to(self.device).unsqueeze(0) # (T_in) -> (1,T_in)
+		encoder_outputs = self.forward_encoder(encoder_x) # (1,T_in,embed_dim)
+		decoder_x = torch.tensor([bos_idx], dtype=torch.long, device=self.device).unsqueeze(0) # (1,1)
+
+		# begin sampling
+		for i in range(max_new_tokens):
+			logits = self.forward_decoder(encoder_outputs, decoder_x) # (1,T,decoder_vocab_size)
+			# cut only to last time step
+			logits = logits[:,-1,:]
+			# apply temperature
+			logits = logits / temperature
+			# apply softmax
+			probs = F.softmax(logits, dim=-1)
+			# sample from the distribution
+			if sampling_strategy == "multinomial":
+				next_idx = torch.multinomial(probs, num_samples=1) # (1,1)
+			elif sampling_strategy == "greedy":
+				next_idx = torch.argmax(probs, dim=-1, keepdim=True) # (1,1)
+			# add sampled idx to the sequence
+			decoder_x = torch.cat([decoder_x, next_idx], dim=-1) # (1,T) -> (1,T+1)
+			# yield the generated token idx
+			token = int(next_idx[0].cpu().numpy())
+			yield token
+			# if the token is EOS, stop generating
+			if token == eos_idx:
+				break 
